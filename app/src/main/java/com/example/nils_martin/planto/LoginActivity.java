c6a3b87a15f4d5dc01cf4a8ef3d7b4a2648de3ea
TheShakiraOfCode <nilsmartin.robeling@gmail.com>
2019-02-17 19:26:17 +0100

    Code cleanup and comment

--- app/src/main/java/com/example/nils_martin/planto/LoginActivity.java
+++ app/src/main/java/com/example/nils_martin/planto/LoginActivity.java
@@ -15,16 +15,13 @@ import android.widget.TextView;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
-import com.facebook.FacebookSdk;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
-import com.facebook.appevents.AppEventsLogger;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.net.URL;
 import java.util.Arrays;
 
 public class LoginActivity extends AppCompatActivity {
@@ -33,23 +30,19 @@ public class LoginActivity extends AppCompatActivity {
     private static final String EMAIL = "email";
     public static final String CHANNEL_1_ID = "Channel 1";
     private final String notificationID = "1234";
-
-
-
-   CallbackManager callbackManager = CallbackManager.Factory.create();
-
-
-   
+    private CallbackManager callbackManager = CallbackManager.Factory.create();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         createNotificationChannel();
-        buildNotification();
         initializeFB();
-
     }
+    
+    /**
+     * Initializes anything facebook related (button, permissions, callbacks, fetching data)
+     * */
     private void initializeFB(){
        loginButton= (LoginButton)findViewById(R.id.login_button);
        loginButton.setReadPermissions(Arrays.asList(EMAIL,"public_profile"));
@@ -58,50 +51,39 @@ public class LoginActivity extends AppCompatActivity {
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                String userID = loginResult.getAccessToken().getUserId();
-
                final ProgressDialog mDialog = new ProgressDialog(LoginActivity.this);
                mDialog.setMessage("Retrieving data... ");
                mDialog.show();
-
-               String accessToken = loginResult.getAccessToken().getToken();
-
 
                 GraphRequest graphRequest =GraphRequest.newMeRequest(loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
                     @Override
                     public void onCompleted(JSONObject object, GraphResponse response) {
                         displayUserinfo(object);
                         mDialog.dismiss();
-                        nmc.notify(Integer.parseInt(notificationID),buildNotification().build());
+                        nmc.notify(Integer.parseInt(notificationID), buildNotificationBuilder("Welcome to Planto","You have" +
+                                " logged in with your facebook account!").build());
 
                     }
                 });
-
                 Bundle parameters = new Bundle();
                 parameters.putString("fields","first_name,last_name,email,id");
                 graphRequest.setParameters(parameters);
                 graphRequest.executeAsync();
-                Intent intent = new Intent(LoginActivity.this,App.class);
-               // startActivity(new Intent(LoginActivity.this,App.class));
-               //
-                // startActivity(intent);
-                //setIntent(new Intent());
-                // App code/
             }
 
             @Override
             public void onCancel() {
-                // App code
             }
 
             @Override
             public void onError(FacebookException exception) {
-                // App code
             }
         });
     }
 
-
+    /**
+     * Fetches the users data and displays it
+     * */
     public void displayUserinfo (JSONObject object){
         String first_name,last_name,email,id;
         first_name="";
@@ -134,21 +116,28 @@ public class LoginActivity extends AppCompatActivity {
         callbackManager.onActivityResult(requestCode, resultCode, data);
         super.onActivityResult(requestCode, resultCode, data);
     }
-    private NotificationCompat.Builder buildNotification(){
+
+    /**
+     * Builds the notification with the text provided
+     * @return the notificationBuilder
+     * */
+    private NotificationCompat.Builder buildNotificationBuilder(String contentTitle,String contentText){
         Intent intent = new Intent(this, LoginActivity.class);
         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
 
         NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_1_ID)
                 .setSmallIcon(R.drawable.ic_notif)
-                .setContentTitle("Welcome to Planto")
-                .setContentText("You have logged in with your facebook account")
+                .setContentTitle(contentTitle)
+                .setContentText(contentText)
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT)
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true);
         return mBuilder;
     }
-
+    /**
+     * Creates a notification channel.
+     * */
     private void createNotificationChannel (){
         //Check if IceCreamSandwich or Api too low
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
